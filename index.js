2540aa047f415a326f23cd28720eeef0f7076b2a
unknown <sumaiyahaque2992@gmail.com>
2024-12-12 15:27:32 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+// const
+const name = "sumaiya";
+const bloodGroup = "A+";
+const today = "new date()";
+const birthday = "4th December";
+const country = "bangladesh";
+const fatherName = "emadul";
+
+// let
+let age = 21;
+let date = 11-12-2024;
+let address = "mathbaria";
+let weight = "50kg";
+let height = 5.3;
+let phoneNumber = 01603337197;
+
+console.log(typeof(date));
---
+++ operators.js
@@ -0,0 +1,6 @@
+
+
+// age declar
+// let age = [ 16, 28, 63, 19, 51, 81, 27, 20, 32, 37];
+let friendList = ["sumaiya", "sadia", "soha", "shara", "suhana"];
+console.log(friendList);
