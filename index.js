208b8f11933373af3b694579950e0b0fd5d242d6
unknown <sumaiyahaque2992@gmail.com>
2024-12-12 15:47:34 +0600

    new file and function

--- index.js
+++ index.js
@@ -5,13 +5,18 @@ const today = "new date()";
 const birthday = "4th December";
 const country = "bangladesh";
 const fatherName = "emadul";
-
+// bollean data type
+const isAdmin = true;
 // let
 let age = 21;
 let date = 11-12-2024;
 let address = "mathbaria";
 let weight = "50kg";
 let height = 5.3;
-let phoneNumber = 01603337197;
+let phoneNumber = "01603337197";
+let salary = 50000;
+
+
+
 
 console.log(typeof(date));
