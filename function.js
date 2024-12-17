f5cc9fbf0df67d2db1efd31b72ebe09f6506fca4
unknown <sumaiyahaque2992@gmail.com>
2024-12-17 16:43:59 +0600

    new file commit function

--- function.js
+++ function.js
@@ -34,8 +34,45 @@
 
 // arrow function
 // let sum = (num1, num2) => num1 + num2;
-let sum = (num1, num2) =>{
-    let total = num1 + num2;
-    return total * total;
+// let sum = (num1, num2) =>{
+//     let total = num1 + num2;
+//     return total * total;
+// }
+// console.log(sum(10, 20));
+
+
+// higher order function
+// function sum(num1, num2, add){
+//     return add(num1, num2)
+// };
+// let add = (number1, number2) => number1 + number2;
+// console.log(sum(30, 20, add))
+
+
+// IIFE
+// (function sum(name){
+//     console.log(name)
+// })("alif")
+
+
+// let x = 5;
+// let y = '5';
+// let z = x + y;
+// console.log(typeof(z))
+
+// let x = 5;
+// x = x + 10;
+// console.log(x);  
+
+// let x = 100;
+// x % = 10;
+
+// for(let i = 0; i <= 100; i++){
+//     if(i % 2 == 1)
+//     console.log(i);
+// }
+
+for(let i = 0; i <= 100; i++){
+    if(i % 2 == 0)
+    console.log(i);
 }
-console.log(sum(10, 20));
---
+++ object.js
@@ -0,0 +1,42 @@
+// Object declar
+
+let person = {
+    firstName: 'sumaiya', 
+    lastName: 'haque',
+    age: 22,
+    isAdmin: true
+};
+// data update
+person.firstName = 'mst. sumaiya';
+// data add
+person.nationality = 'Bangladeshi';
+// data delete
+delete person.lastName;
+console.log(person);
+// console.log(person.firstName);
+
+
+
+
+// let employe = {
+//     employeName: 'akkas',
+//     jobTitle: 'officer',
+// nasted Object
+//     skill: {
+//           course1: {
+//             skill: 'web development',
+//             duration: '6 month'
+//           },
+//           course2: {
+//             skill: 'digital marketing',
+//             duration: '3 month'
+//           },
+//           course3: {
+//             skill: 'graphic design',
+//             duration: '5 month'
+//           }
+//     },
+//     salary: 40000
+// }
+// console.log(employe.skill.course2.duration);
+
